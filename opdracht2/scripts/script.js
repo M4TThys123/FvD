57d09053c52dfb9c68b9b0ca65c2773695da7832
M4TThys123 <matthijs.blauw2@gmail.com>
2021-06-07 16:06:22 +0200

    Versie 1 testen mobiel

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("howdy");
+
+// Filter Systeem
+// Bron https://codepen.io/shooft/pen/VwpvVYy
+var optieAll = document.querySelector("#filter-all");
+var optieGalaxy = document.querySelector("#filter-galaxy");
+var optieSolar = document.querySelector("#filter-solar");
+
+function filterLijst(event){
+  let deLijst = document.querySelector("ul");
+  let nieuweFilter = event.target.value;
+  deLijst.className = "";
+  deLijst.classList.add(nieuweFilter);
+}
+
+/* de radio buttons laten luisteren naar wijzigingen */
+/* dan de functie filterList uitvoeren */
+optieAll.addEventListener("change", filterLijst);
+optieGalaxy.addEventListener("change", filterLijst);
+optieSolar.addEventListener("change", filterLijst);
